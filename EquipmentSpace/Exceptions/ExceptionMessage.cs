9da38dccd7c928300b97d9655645bdcbb22a892e
“ <61949378+FoldeR-ua@users.noreply.github.com>
2023-02-20 21:02:42 +0200

    Updated

--- EquipmentSpace/Exceptions/ExceptionMessage.cs
+++ EquipmentSpace/Exceptions/ExceptionMessage.cs
@@ -2,8 +2,8 @@
 {
     public static class ExceptionMessage
     {
-        public static readonly string spaceNotFoundMessage = "Space wasn't found . . . \n Check the ID of the space";
-        public static readonly string equipmentNotFoundMessage = "Equipment wasn't found . . . \n Check the ID of the equipment";
-        public static readonly string spaceMessage = "There isn't enought space for this contract . . .";
+        public static readonly string spaceNotFoundMessage = "Space wasn't found\n Check the CODE of the space";
+        public static readonly string equipmentNotFoundMessage = "Equipment wasn't found\n Check the CODE of the equipment";
+        public static readonly string spaceMessage = "There isn't enought space for this contract";
     }
 }
